507699220ec655a62df0d3809ad74ec38edbbeef
Dev-Solution <bogdanov.sergey1991@gmail.com>
2016-02-13 14:53:19 +0200

    first start Hibernate

--- src/main/java/start/MainForm.java
+++ src/main/java/start/MainForm.java
@@ -1,13 +1,17 @@
-package start; /**
+package start;
+/**
  * Created by Sergey on 31.01.2016.
  */
 
 
+import Util.HibernateUtil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.GoodType;
+import org.hibernate.Session;
 
 public class MainForm extends Application {
 
@@ -18,10 +22,27 @@ public class MainForm extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         String fxmlFile = "/view/MainForm.fxml";
-        FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResource(fxmlFile));
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
         stage.setTitle("TorgSolution");
         stage.setScene(new Scene(root));
         stage.show();
+        /*try first start Hibernate */
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        GoodType goodType1 = new GoodType();
+        goodType1.setName("Chocolate");
+        session.save(goodType1);
+
+        session.getTransaction().commit();
+
+        session.beginTransaction();
+        GoodType goodTypeFromDB = (GoodType) session.load(GoodType.class,goodType1.getId());
+        System.out.println(goodTypeFromDB.getName());
+        session.getTransaction().commit();
+
+        HibernateUtil.shutdown();
+        System.out.println("Done");
     }
 }
